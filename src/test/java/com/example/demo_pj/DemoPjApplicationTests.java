52867beb17741140967f15d9e9cab057867dd5f6
heeseo333 <heeseo310@gmail.com>
2024-05-21 09:29:06 +0900

    first commit

---
+++ src/test/java/com/example/demo_pj/DemoPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
